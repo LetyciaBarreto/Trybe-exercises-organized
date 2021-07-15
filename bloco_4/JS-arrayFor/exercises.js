6771487c86d63a997ebb852cb772a19e324c7b04
Letycia Barreto <letyciasbarreto@gmail.com>
2021-07-15 18:07:38 -0300

    exercicio 2 e 3

--- bloco_4/JS-arrayFor/exercises.js
+++ bloco_4/JS-arrayFor/exercises.js
@@ -1,6 +1,26 @@
 //Exe 01
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
+ for(i = 0; i < numbers1.length; i++) {
+   console.log(numbers1[i]);
+ }
+
+//Exe 02
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for(i = 0; i < numbers2.length; i++) {
+ sum += numbers2[i];
+  }
+  console.log(sum);
+
+  //Exe 03
+let numbers3 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum3 = 0;
+let med = 0;
+
+for(i = 0; i < numbers3.length; i++) {
+ sum3 += numbers3[i];
+ med = sum3 / numbers3.length;
+  }
+  console.log(med);
